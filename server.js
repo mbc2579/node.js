1f775173108194bf20243361a8349634d33e5316
mbc2579 <mbc2579@naver.com>
2024-02-22 16:49:22 +0900

    채팅기능 만들기 2 (채팅방/상세페이지)

--- server.js
+++ server.js
@@ -402,3 +402,23 @@ app.post('/comment', async (요청, 응답) => {
 // app.get('*', function(요청, 응답) {
   // 응답.sendFile(path.join(__dirname, 'react-project/build/index.html'));
 // })
+
+app.get('/chat/request', async (요청, 응답)=> {
+  await db.collection('chatroom').insertOne({
+    member : [요청.user._id, new ObjectId(요청.query.writerId)],
+    date : new Date()
+  })
+  응답.redirect('/chat/list')
+})
+
+app.get('/chat/list', async (요청, 응답)=> {
+  let result = await db.collection('chatroom').find({
+    member : 요청.user._id
+  }).toArray()
+  응답.render('chatList.ejs', {result : result})
+})
+
+app.get('/chat/detail/:id', async (요청, 응답)=> {
+  let result = await db.collection('chatroom').findOne({_id : new ObjectId(요청.params.id)})
+  응답.render('chatDetail.ejs', {result : result})
+})
